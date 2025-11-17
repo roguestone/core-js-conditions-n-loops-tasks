2ccf8b19940b69d199efb3370fc00b6b2c0ce876
Gnarkill33 <gnarkill33@gmail.com>
2025-11-17 22:13:05 +0300

    feat: implement isPalindrome problem

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -231,8 +231,16 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const middle = Math.floor(str.length / 2);
+
+  for (let i = 0; i < middle; i += 1) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
